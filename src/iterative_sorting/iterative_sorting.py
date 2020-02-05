4c21eed59d2ebd2d4ec545137f255529317dd11f
Alyssa Saez <xcthaeh@gmail.com>
2020-02-05 21:48:18 +0100

    Working on iterative sorting, one test failing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,13 +5,12 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-
+            arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
 
 
@@ -20,6 +19,13 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i
+        while j > 0 and temp < arr[j - 1]:
+            arr[j] = arr[j - 1]
+            j -= 1
+            arr[j] = temp
 
     return arr
 
